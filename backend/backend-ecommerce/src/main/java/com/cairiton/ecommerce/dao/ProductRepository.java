292255772e6ce358de4c0417eb5d2c77888fd0f7
joaocairiton <joaocairiton@gmail.com>
2023-07-10 15:11:11 -0300

    project angular created

---
+++ backend/backend-ecommerce/src/main/java/com/cairiton/ecommerce/dao/ProductRepository.java
@@ -0,0 +1,11 @@
+package com.cairiton.ecommerce.dao;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.cairiton.ecommerce.entity.Product;
+
+
+
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+}
